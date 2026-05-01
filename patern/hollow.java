8aa3a555aae9873eb44caa41e4340633ce3468f8
Anurag srivastava <anuragsrivastava8874@gmail.com>
2026-05-02 01:06:11 +0530

    updating hollow rectangel

---
+++ patern/hollow.java
@@ -0,0 +1,29 @@
+public class hollow{
+
+public static void RectangleTohollow(int row,   int cols){
+
+for(int i = 1 ; i <= row; i++){
+    for(int j = 1 ; j <= cols; j++ ){
+
+        if (i==1 || i==row || j == 1 || j == cols){
+             System.out.print("*");
+        } else{
+             System.out.print(" ");
+        }
+    
+    }
+    System.out.println();
+}
+
+}
+
+
+
+
+public static void main(String[] args) {
+RectangleTohollow(4,5);
+    
+}
+
+
+}
