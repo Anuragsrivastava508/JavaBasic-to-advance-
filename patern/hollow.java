b8908e3abf5a097a94e5ad585774a457f623e77f
Anurag srivastava <anuragsrivastava8874@gmail.com>
2026-05-04 23:39:10 +0530

    update

--- patern/hollow.java
+++ patern/hollow.java
@@ -47,17 +47,18 @@ for(int i = 1 ; i <= row; i++){
 // 4 5 6 
 // 7 8 9 10 
 // 11 12 13 14 15
-// public static void Floyd_triangle(int n){
-//     int counter = 1 ;
-//     // outer loop
-//     for(int i = 1; i<= n; i++){
-//         for (int j = 1; j <= i; j++) {
-//             System.out.print(counter+ " ");
-//             counter++;
-//         }
-//         System.out.println();
-//     }
-// }
+
+public static void Floyd_triangle(int n){
+    int counter = 1 ;
+    // outer loop
+    for(int i = 1; i<= n; i++){
+        for (int j = 1; j <= i; j++) {
+            System.out.print(counter+ " ");
+            counter++;
+        }
+        System.out.println();
+    }
+}
 
 
 // 1
