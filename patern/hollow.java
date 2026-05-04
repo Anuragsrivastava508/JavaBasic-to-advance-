a5501a087001533d3e26480aa02c5eb46809d105
Anurag srivastava <anuragsrivastava8874@gmail.com>
2026-05-04 23:39:59 +0530

    done

--- patern/hollow.java
+++ patern/hollow.java
@@ -66,21 +66,22 @@ public static void Floyd_triangle(int n){
 // 1 0 1
 // 0 1 0 1
 // // 1 0 1 0 1
-// public static  void zero_one_triangle(int n){
-//     // outer loop
-//     for(int i = 1; i<= n; i++){
-//         // inner loop
-//         for (int j = 1; j <= i; j++) {
-//             if ((i+j) % 2 == 0) {
-//                 System.out.print("1");
-//             } else {
-//                 System.out.print("0");
-//             }
-//         }
 
-//         System.err.println();
-//     }
-// }
+public static  void zero_one_triangle(int n){
+    // outer loop
+    for(int i = 1; i<= n; i++){
+        // inner loop
+        for (int j = 1; j <= i; j++) {
+            if ((i+j) % 2 == 0) {
+                System.out.print("1");
+            } else {
+                System.out.print("0");
+            }
+        }
+
+        System.err.println();
+    }
+}
 
 // public static void  Butterfly(int n){
 //     // outer loop
