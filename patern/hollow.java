b2f0902de2cb9728f829e28e11ae2f9e0c1f57f6
Anurag srivastava <anuragsrivastava8874@gmail.com>
2026-05-04 23:38:19 +0530

    updating

--- patern/hollow.java
+++ patern/hollow.java
@@ -1,24 +1,24 @@
 public class hollow{
 
-// // * * * * * *
-// // *         *
-// // *         *
-// // * * * * * *
+// * * * * * *
+// *         *
+// *         *
+// * * * * * *
 
-// public static void RectangleTohollow(int row,   int cols){
-// for(int i = 1 ; i <= row; i++){
-//     for(int j = 1 ; j <= cols; j++ ){
+public static void RectangleTohollow(int row,   int cols){
+for(int i = 1 ; i <= row; i++){
+    for(int j = 1 ; j <= cols; j++ ){
 
-//         if (i==1 || i==row || j == 1 || j == cols){
-//              System.out.print("*");
-//         } else{
-//              System.out.print(" ");
-//         }
+        if (i==1 || i==row || j == 1 || j == cols){
+             System.out.print("*");
+        } else{
+             System.out.print(" ");
+        }
     
-//     }
-//     System.out.println();
-// }
-// }
+    }
+    System.out.println();
+}
+}
 
 //         *
 //       * *
