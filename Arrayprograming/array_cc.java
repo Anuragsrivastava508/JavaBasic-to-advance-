e3288ff591a7cfef90046f5ce4789e43651d88b0
Anurag srivastava <anuragsrivastava8874@gmail.com>
2026-05-11 01:08:32 +0530

    update

--- Arrayprograming/array_cc.java
+++ Arrayprograming/array_cc.java
@@ -1,10 +1,13 @@
- public class array_cc{
+
+
+public class array_cc{
  
  
 //  finding the lenght of an array 
     public static void main(String[] args) {
-    int mark[] =new mark[20];
-    System.out.println("Lenght of an array " + mark.length);
+    // int mark[] =new mark[20];
+    int arr[] = new int[5];
+    System.out.println("Lenght of an array " + arr.length);
  }
 
  }
