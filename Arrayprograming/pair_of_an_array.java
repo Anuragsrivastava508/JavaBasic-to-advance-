392e7519efff0eb378294ca323fc1c1a637b5610
Anurag srivastava <anuragsrivastava8874@gmail.com>
2026-05-23 08:47:52 +0530

    done

---
+++ Arrayprograming/pair_of_an_array.java
@@ -0,0 +1,64 @@
+public class pair_of_an_array {
+  
+    public static void paires(int number[]){
+          
+        int tp = 0;
+        
+        for (int i = 0; i < number.length; i++) {
+        
+            int currnt = number [i];
+        
+            for (int j = i + 1 ; j < number.length; j++) {
+        
+                System.out.print("( " + currnt + " " + number[j] + ")");
+        
+                tp++;
+            }
+        
+            System.out.println();
+}
+              System.out.println("total of the number " + tp);
+}
+    public static void main(String[] args) {
+        
+        int[] numbers = {1, 2, 3, 4,5,6,7,8,9,10};
+        
+        paires(numbers);
+    
+    }
+
+}
+
+
+
+
+// public class PairOfArray {
+
+//     public static void pairs(int numbers[]) {
+
+//         int totalPairs = 0;
+
+//         for (int i = 0; i < numbers.length; i++) {
+
+//             int current = numbers[i];
+
+//             for (int j = i + 1; j < numbers.length; j++) {
+
+//                 System.out.print("(" + current + " " + numbers[j] + ") ");
+
+//                 totalPairs++;
+//             }
+
+//             System.out.println();
+//         }
+
+//         System.out.println("Total Pairs = " + totalPairs);
+//     }
+
+//     public static void main(String[] args) {
+
+//         int[] numbers = {1, 2, 3, 4};
+
+//         pairs(numbers);
+//     }
+// }
