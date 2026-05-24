d3e80707b49841e19b8cdb700b6158c951ebddcd
Anurag srivastava <anuragsrivastava8874@gmail.com>
2026-05-24 09:43:04 +0530

    done

--- Arrayprograming/array__ps.java
+++ Arrayprograming/array__ps.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class array__ps {
 //   pring the input and increas the out put by 1
 
-// public static void Printing_Input(){
-//     Scanner SC = new Scanner(System.in);
-//     int marks [] = new int[3];
-//     System.out.println("enter your number");
-// for (int i = 0; i < marks.length; i++) {
-//     marks[i] = SC.nextInt();
-//     marks[i] = marks[i] + 1;
-// }
-// System.out.println("the marks of phy :" + marks[0]);
-// System.out.println("the marks of MATH :" + marks[1]);
-// System.out.println("the marks of CHEM :" + marks[2]);
+public static void Printing_Input(){
+    Scanner SC = new Scanner(System.in);
+    int marks [] = new int[3];
+    System.out.println("enter your number");
+for (int i = 0; i < marks.length; i++) {
+    marks[i] = SC.nextInt();
+    marks[i] = marks[i] + 1;
+}
+System.out.println("the marks of phy :" + marks[0]);
+System.out.println("the marks of MATH :" + marks[1]);
+System.out.println("the marks of CHEM :" + marks[2]);
 
-// }
+}
+
+
 public static int Linear_Search(int num[], int key) {
     for (int i = 0; i < num.length; i++) {
         if (num[i] == key) {
@@ -41,7 +43,7 @@ public static void Largest_Number(int num[]){
 } 
 
     public static void main(String[] args) {
-    //   Printing_Input();
+      Printing_Input();
     int num[] = {1,2,3,4,5,6,7,8,9};
     Largest_Number(num);
     int key = 8;
